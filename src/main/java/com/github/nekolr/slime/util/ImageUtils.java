89b6efa456a3f4e379b9a183bcc5b5ba016d707b
nekolr <excalibll@163.com>
2021-07-12 22:37:39 +0800

    🐛 fix thumbnails destination

--- src/main/java/com/github/nekolr/slime/util/ImageUtils.java
+++ src/main/java/com/github/nekolr/slime/util/ImageUtils.java
@@ -1,7 +1,6 @@
 package com.github.nekolr.slime.util;
 
 import net.coobird.thumbnailator.Thumbnails;
-import net.coobird.thumbnailator.name.Rename;
 
 import java.io.*;
 
@@ -14,17 +13,17 @@ public class ImageUtils {
 
     public static void compressImage(File file, int ltLimit, int gtLimit) throws IOException {
         if (file.length() > ltLimit && file.length() < gtLimit) {
-            compressImage(new FileInputStream(file), 0.9f, 1.0f);
+            compressImage(new FileInputStream(file), file, 0.95f, 1.0f);
         } else if (file.length() > gtLimit) {
-            compressImage(new FileInputStream(file), 0.8f, 1.0f);
+            compressImage(new FileInputStream(file), file, 0.8f, 1.0f);
         }
     }
 
-    public static void compressImage(InputStream input, float scale, float quality) throws IOException {
+    public static void compressImage(InputStream input, File destination, float scale, float quality) throws IOException {
         Thumbnails.of(input)
                 .scale(scale)
                 .outputQuality(quality)
-                .toFiles(Rename.NO_CHANGE);
+                .toFile(destination);
     }
 
 }

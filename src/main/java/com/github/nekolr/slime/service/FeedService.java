9dc161008d5d58ef87bd9b28518b05326cf26571
nekolr <excalibll@163.com>
2022-03-18 14:40:15 +0800

    🚧 drop base service

--- src/main/java/com/github/nekolr/slime/service/FeedService.java
+++ src/main/java/com/github/nekolr/slime/service/FeedService.java
@@ -5,15 +5,19 @@ import com.github.nekolr.slime.domain.Feed;
 import java.util.Date;
 import java.util.List;
 
-public interface FeedService extends BaseService<Feed> {
+public interface FeedService {
+
+    Feed save(Feed entity);
 
     Boolean existsByGuid(String guid);
 
     List<Feed> findByGuid(String guid);
 
+    void deleteInBatch(List<Feed> entries);
+
+    List<Feed> findByCreateTimeLessThanEqual(Date time);
+
     List<Feed> findByGuidAndPushed(String guid, Boolean pushed);
 
     List<Feed> findByCategoryAndPushed(String group, Boolean pushed);
-
-    List<Feed> findByCreateTimeLessThanEqual(Date time);
 }
